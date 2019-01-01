f4940feb03c3c6a7c81ec23f6258016902aff1f1
kitaz <redcrow.urawa@gmail.com>
2019-01-01 22:18:26 +0900

    Tuple type Training

--- Sample8.py
+++ Sample8.py
@@ -1,12 +1,23 @@
+# List
 data1 = [1,2,3,4,5]
 data2 = list(data1)
 
-print("data1は", data1, "です。")
-print("data2は", data2, "です。")
+print("List data1は", data1, "です。")
+print("List data2は", data2, "です。")
 
 data1[0] = 10
 
-print("data1を変更します。")
+print("List data1を変更します。")
 
-print("data1は", data1, "です。")
-print("data2は", data2, "です。")
+print("List data1は", data1, "です。")
+print("List data2は", data2, "です。")
+
+
+# Tuple
+data3 = (1,2,3,4,5)
+
+print("Tuple data3は", data3, "です。")
+
+data3[0] = 10
+
+print("Tuple data3は", data3, "です。")
